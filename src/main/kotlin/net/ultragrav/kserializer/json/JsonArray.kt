c2e797efafb560966993639d93857991b5430658
UltraDev <petzmagnus@gmail.com>
2023-05-26 10:34:24 +0200

    Add locks to JsonObject and JsonArray

--- src/main/kotlin/net/ultragrav/kserializer/json/JsonArray.kt
+++ src/main/kotlin/net/ultragrav/kserializer/json/JsonArray.kt
@@ -1,7 +1,28 @@
 package net.ultragrav.kserializer.json
 
+import java.util.concurrent.locks.ReentrantReadWriteLock
+
 class JsonArray() : JsonIndexable<Int> {
     private val backingList: MutableList<Any?> = mutableListOf()
+
+    private val lock: ReentrantReadWriteLock = ReentrantReadWriteLock()
+    private inline fun <T> readLocked(block: () -> T): T {
+        try {
+            lock.readLock().lock()
+            return block()
+        } finally {
+            lock.readLock().unlock()
+        }
+    }
+
+    private inline fun <T> writeLocked(block: () -> T): T {
+        try {
+            lock.writeLock().lock()
+            return block()
+        } finally {
+            lock.writeLock().unlock()
+        }
+    }
 
     val size get() = backingList.size
 
@@ -9,61 +30,51 @@ class JsonArray() : JsonIndexable<Int> {
         backingList.addAll(list)
     }
 
-    override fun getString(key: Int): String {
-        return backingList[key] as String
-    }
+    override fun getString(key: Int): String = readLocked { return backingList[key] as String }
 
-    override fun setString(key: Int, value: String): Any? {
+    override fun setString(key: Int, value: String): Any? = writeLocked {
         growToAccommodate(key)
         return backingList.set(key, value)
     }
 
-    fun addString(value: String, index: Int = -1) {
+    fun addString(value: String, index: Int = -1) = writeLocked {
         if (index == -1)
             backingList.add(value)
         else
             backingList.add(index, value)
     }
 
-    override fun getObject(key: Int): JsonObject {
-        return backingList[key] as JsonObject
-    }
+    override fun getObject(key: Int): JsonObject = readLocked { return backingList[key] as JsonObject }
 
-    override fun setObject(key: Int, data: JsonObject): Any? {
+    override fun setObject(key: Int, data: JsonObject): Any? = writeLocked {
         growToAccommodate(key)
         return backingList.set(key, data)
     }
 
-    fun addObject(data: JsonObject, index: Int = -1) {
+    fun addObject(data: JsonObject, index: Int = -1) = writeLocked {
         if (index == -1)
             backingList.add(data)
         else
             backingList.add(index, data)
     }
 
-    override fun getArray(key: Int): JsonArray {
-        return backingList[key] as JsonArray
-    }
+    override fun getArray(key: Int): JsonArray = readLocked { return backingList[key] as JsonArray }
 
-    override fun setArray(key: Int, array: JsonArray): Any? {
+    override fun setArray(key: Int, array: JsonArray): Any? = writeLocked {
         growToAccommodate(key)
         return backingList.set(key, array)
     }
 
-    fun addArray(array: JsonArray, index: Int = -1) {
+    fun addArray(array: JsonArray, index: Int = -1) = writeLocked {
         if (index == -1)
             backingList.add(array)
         else
             backingList.add(index, array)
     }
 
-    fun remove(index: Int) {
-        backingList.removeAt(index)
-    }
+    fun remove(index: Int) = writeLocked { backingList.removeAt(index) }
 
-    fun clear() {
-        backingList.clear()
-    }
+    fun clear() = writeLocked { backingList.clear() }
 
     private fun growToAccommodate(index: Int) {
         while (backingList.size <= index) {
--- src/main/kotlin/net/ultragrav/kserializer/json/JsonObject.kt
+++ src/main/kotlin/net/ultragrav/kserializer/json/JsonObject.kt
@@ -1,27 +1,47 @@
 package net.ultragrav.kserializer.json
 
+import java.util.concurrent.locks.ReentrantReadWriteLock
+
 class JsonObject : JsonIndexable<String> {
     private val backingMap = mutableMapOf<String, Any>()
 
+    private val lock: ReentrantReadWriteLock = ReentrantReadWriteLock()
+    private inline fun <T> readLocked(block: () -> T): T {
+        try {
+            lock.readLock().lock()
+            return block()
+        } finally {
+            lock.readLock().unlock()
+        }
+    }
+    private inline fun <T> writeLocked(block: () -> T): T {
+        try {
+            lock.writeLock().lock()
+            return block()
+        } finally {
+            lock.writeLock().unlock()
+        }
+    }
+
     override fun getString(key: String): String {
-        return backingMap[key] as String
+        readLocked { return backingMap[key] as String }
     }
     override fun setString(key: String, value: String): Any? {
-        return backingMap.put(key, value)
+        writeLocked { return backingMap.put(key, value) }
     }
 
     override fun getObject(key: String): JsonObject {
-        return backingMap[key] as JsonObject
+        readLocked { return backingMap[key] as JsonObject }
     }
     override fun setObject(key: String, data: JsonObject): Any? {
-        return backingMap.put(key, data)
+        writeLocked { return backingMap.put(key, data) }
     }
 
     override fun getArray(key: String): JsonArray {
-        return backingMap[key] as JsonArray
+        readLocked { return backingMap[key] as JsonArray }
     }
     override fun setArray(key: String, array: JsonArray): Any? {
-        return backingMap.put(key, array)
+        writeLocked { return backingMap.put(key, array) }
     }
 
     fun contains(key: String): Boolean {
